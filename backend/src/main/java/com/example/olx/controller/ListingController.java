fd9091e4e649fff034786f0ca3c63d8ed42f8cb6
Stefan Vieru <stefan.vieru@student.tuiasi.ro>
2024-05-27 19:28:12 +0300

    fix

--- backend/src/main/java/com/example/olx/controller/ListingController.java
+++ backend/src/main/java/com/example/olx/controller/ListingController.java
@@ -1,7 +1,6 @@
 package com.example.olx.controller;
 
 import com.example.olx.aws_config.SendEmail;
-import com.example.olx.aws_config.SnsConfig;
 import com.example.olx.entity.Listing;
 import com.example.olx.service.ListingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,28 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.nio.file.Paths;
+
 @RestController
 @RequestMapping
 public class ListingController {
     private final ListingService listingService;
     private final S3Client s3Client;
-    private final SendEmail sns;
     @Autowired
 
-    public ListingController(ListingService listingService, S3Client s3Client, SendEmail sns) {
+    public ListingController(ListingService listingService, S3Client s3Client) {
         this.listingService = listingService;
         this.s3Client = s3Client;
-        this.sns = sns;
     }
+
     @PostMapping("/")
-    public Listing create(@RequestBody Listing listing){
+    public void create(@RequestBody Listing listing){
         //s3
         listingService.create(listing);
         PutObjectRequest objReq = PutObjectRequest.builder()
-                .bucket("")
-                .key()
+                .bucket("oferte-directe-bucket")
+                .key("hello.txt")
                 .build();
-        s3Client.putObject(objReq, software.amazon.awssdk.core.sync.RequestBody.fromInputStream(cv.getInputStream(), cv.getInputStream().available()));
+        s3Client.putObject(objReq, Paths.get("hello.txt"));
 
     }
 }
