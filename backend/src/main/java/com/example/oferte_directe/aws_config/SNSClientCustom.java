7c45dbf51c5e26bd92fee22b73ff79be6b1be9e3
Stefan Vieru <stefan.vieru@student.tuiasi.ro>
2024-05-28 15:30:43 +0300

    add sns

---
+++ backend/src/main/java/com/example/oferte_directe/aws_config/SNSClientCustom.java
@@ -0,0 +1,27 @@
+package com.example.oferte_directe.aws_config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+
+@Configuration
+public class SNSClientCustom {
+    private final SnsClient snsClient;
+    private final String topic;
+    @Autowired
+    SNSClientCustom(SnsClient snsClient){
+        this.snsClient = snsClient;
+        this.topic = "oferte-directe-bucket";
+    }
+
+    public void send_sns_message(String message){
+        PublishRequest publishRequest = PublishRequest.builder()
+                .message(message)
+                .topicArn(this.topic)
+                .build();
+        snsClient.publish(publishRequest);
+    }
+}
