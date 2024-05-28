6efa10e0c6c35ea63550b0a208b05acb2b59b5eb
Stefan Vieru <stefan.vieru@student.tuiasi.ro>
2024-05-28 11:18:43 +0300

    add cors

---
+++ backend/src/main/java/com/example/oferte_directe/security/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.example.oferte_directe.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://http://54.243.193.76:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*");
+    }
+}
