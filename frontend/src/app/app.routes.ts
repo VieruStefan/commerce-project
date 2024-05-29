b71f3d41b312b6313c1bef7078cf9cf33efd0f7f
Stefan Vieru <stefan.vieru@student.tuiasi.ro>
2024-05-29 12:54:13 +0300

    small update

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { CreateListingComponent } from '../create-listing/create-listing.component';
+import { IndexComponent } from '../index/index.component';
 
 export const routes: Routes = [
-    { path: "", component: HomeComponent},
+    { path: "", component: IndexComponent},
+    { path: "home", component: HomeComponent},
     { path: "add", component: CreateListingComponent}
 ];
